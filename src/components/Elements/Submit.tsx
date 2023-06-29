ae1dc2d0f2f26beabacbc1fb86d49a03e8ca30a1
Sylvain Tshiasuma <sylvain.tshiasuma2017@gmail.com>
2023-06-29 17:52:37 +0100

    added loader on submit

--- src/components/Elements/Submit.tsx
+++ src/components/Elements/Submit.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { classNamesConcat } from '../../lib/utils';
 import { ClassNames } from '../../types';
+import { TailSpin } from 'react-loader-spinner';
 
 interface Props {
   label?: string;
@@ -8,15 +9,21 @@ interface Props {
 }
 
 export const Submit: FC<Props> = ({ classNames, label }) => {
+  const [onClick, setOnClick] = useState(false);
   return (
     <button
       type="submit"
+      onClick={() => setOnClick(true)}
       className={classNamesConcat(
         classNames?.button ||
           'inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white border border-transparent rounded-md shadow-sm bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500'
       )}
     >
-      {label || 'Submit'}
+      {onClick ? (
+        <TailSpin color="#1f2937" height={15} width={15} />
+      ) : (
+        label || 'Submit'
+      )}
     </button>
   );
 };
